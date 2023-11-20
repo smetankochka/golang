b8b570cf054a35ec8cce8dc6f9d620f6b9041604
smetanka <148964894+smetankochka@users.noreply.github.com>
2023-11-20 15:33:24 +0700

    Create is_power_of_two_rec.go

---
+++ edu/function/is_power_of_two_rec.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func IsPowerOfTwoRecursive(N int) {
+	if N == 2 || N == 1{
+		fmt.Println("YES")
+	} else if N % 2 == 1 {
+		fmt.Println("NO")
+	} else {
+		IsPowerOfTwoRecursive(N / 2)
+	}
+}
