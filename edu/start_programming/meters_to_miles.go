3eea3b95cd422ec538d69b233936cbd03f96c1d4
smetanka <148964894+smetankochka@users.noreply.github.com>
2023-11-20 15:11:34 +0700

    Create meters_to_miles.go

---
+++ edu/start_programming/meters_to_miles.go
@@ -0,0 +1,9 @@
+package main
+import "fmt"
+func main() {
+    var meters float32 = 0.0
+    fmt.Scan(&meters)
+    const exchangeRate = 1852
+    seaMiles := meters / exchangeRate
+    fmt.Println(seaMiles)
+}
