f9c195d3ca1766e0874b0cc1954f117f69060f5f
smetanka <mmura997@yandex.by>
2023-11-23 21:16:32 +0700

    add struct and go.mod

---
+++ edu/structure/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"lesson/students"
+)
+
+func main() {
+	stud := students.Student{"Kate", 8}
+	fmt.Println(stud)
+}
---
+++ edu/structure/students/students.go
@@ -0,0 +1,6 @@
+package students
+
+type Student struct {
+	Name string
+	Year int
+}
