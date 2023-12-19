b02c68e539c95550eafb2b9d1fb3afc3931b9daa
smetanka <smetanochka@petalmail.com>
2023-12-19 13:44:56 +0700

    upd

--- edu/errors_panic_type/get_character_at_position.go
+++ edu/errors_panic_type/get_character_at_position.go
@@ -1,18 +1,10 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
+import "errors"
 
 func GetCharacterAtPosition(str string, position int) (rune, error) {
-
 	if position < 0 || position >= len(str) {
 		return '0', errors.New("position out of range")
 	}
 	return []rune(str)[position], nil
-}
-
-func main() {
-	fmt.Println(GetCharacterAtPosition("Зенит чемпион!", 20))
 }
